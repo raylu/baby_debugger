3ced38a8ef8f3d90889ea404d5f47d477736cc2d
raylu <90059+raylu@users.noreply.github.com>
2024-03-28 21:13:38 -0700

    restructure nap components to show summary

--- ts/baby_day.ts
+++ ts/baby_day.ts
@@ -1,6 +1,6 @@
 import {Task} from '@lit/task';
 import {html, css, LitElement} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
 
 interface DayNaps {
 	baby: {'name': string};
@@ -13,16 +13,34 @@ interface Nap {
 	calm_down_time: number;
 }
 
+const napUpdated = new Event('nap-updated', {composed: true});
+
 @customElement('baby-day')
 export class BabyDay extends LitElement {
 	@property({type: String})
 	day = '';
 
+	@state()
+	naps: NapSection[] = [];
+
+	constructor() {
+		super();
+		this.addEventListener('nap-updated', () => this.requestUpdate());
+	}
+
 	private _readTask = new Task(this, {
 		task: async ([day], {signal}) => {
 			const response = await fetch('/api/baby/1/day/' + day, {signal});
 			if (!response.ok) throw new Error(`${response.status} ${response.statusText}`);
-			return await response.json() as DayNaps;
+			const dayNaps = await response.json() as DayNaps;
+			this.naps = [
+				this._renderNap(1, dayNaps.naps[1], 75),
+				this._renderNap(2, dayNaps.naps[2], 90),
+				this._renderNap(3, dayNaps.naps[3], 90),
+				this._renderNap(4, dayNaps.naps[4], 90),
+				this._renderNap(5, dayNaps.naps[3], 105),
+			];
+			return dayNaps;
 		},
 		args: () => [this.day]
 	});
@@ -30,31 +48,39 @@ export class BabyDay extends LitElement {
 	render() {
 		const inner = this._readTask.render({
 			pending: () => html`loading...`,
-			complete: (dayNaps) => html`
-				<section>
-					<h2>${dayNaps.baby.name} &mdash; ${this.day}</h2>
-					<div>nap 1</div>
-					<div>nap 2</div>
-					<div>nap 3</div>
-					<div>nap 4</div>
-					<div>night</div>
+			complete: (dayNaps) => {
+				return html`
+					<section>
+						<h2>${dayNaps.baby.name} &mdash; ${this.day}</h2>
+						<div>nap 1 (${this.naps[0].sleepTime} - ${this.naps[1].wakeUpTime})</div>
+						<div>nap 2 (${this.naps[1].sleepTime} - ${this.naps[2].wakeUpTime})</div>
+						<div>nap 3 (${this.naps[2].sleepTime} - ${this.naps[3].wakeUpTime})</div>
+						<div>nap 4 (${this.naps[3].sleepTime} - ${this.naps[4].wakeUpTime})</div>
+						<div>night</div>
 
-					<div>total naptime</div>
-					<div>total awake time</div>
-				</section>
-				${this._renderNap(1, dayNaps.naps[1], 75)}
-				${this._renderNap(2, dayNaps.naps[2], 90)}
-				`,
+						<div>total naptime</div>
+						<div>total awake time</div>
+					</section>
+					${this.naps}
+				`
+			},
 			error: (e) => html`${e}`
 		});
 		return html`<section>${inner}</section>`;
 	}
 
 	private _renderNap(number: number, nap: Nap | undefined, defaultAwakeWindow: number) {
-		return html`
-			<nap-section day="${this.day}" number="${number}" wakeUpTime="${nap?.wake_up_time}"
-				awakeWindow="${nap?.awake_window ?? defaultAwakeWindow}" calmDown="${nap?.calm_down_time}"></nap-section>
-		`;
+		const napSection = new NapSection();
+		napSection.day = this.day;
+		napSection.number = number;
+		if (nap) {
+			napSection.wakeUpTime = nap.wake_up_time;
+			napSection.awakeWindow = nap.awake_window;
+			napSection.calmDown = nap.calm_down_time;
+			napSection.estimate(null);
+		} else
+			napSection.awakeWindow = defaultAwakeWindow;
+		return napSection;
 	}
 
 	static styles = css`
@@ -102,10 +128,11 @@ export class NapSection extends LitElement {
 		this.calmDown = Number.parseInt((event.target as HTMLInputElement).value);
 	}
 
-	private _estimate(_event: Event) {
+	estimate(_event: Event | null) {
 		const wakeUpDate = new Date(`${this.day}T${this.wakeUpTime}`);
 		this.sleepTime = this._formatTime(wakeUpDate, this.awakeWindow);
 		this.putDownTime = this._formatTime(wakeUpDate, this.awakeWindow - this.calmDown);
+		this.dispatchEvent(napUpdated);
 	}
 
 	private _formatTime(date: Date, deltaMins: number) {
@@ -129,7 +156,7 @@ export class NapSection extends LitElement {
 						<input type="range" value="${this.calmDown}" max="60" @change="${this._calmDownChange}">
 						${this.calmDown} minutes
 					</label>
-					<input type="button" value="→" @click="${this._estimate}">
+					<input type="button" value="→" @click="${this.estimate}">
 					<label>estimated baby sleep time<input readonly value="${this.sleepTime}"></label>
 					<label>estimated baby put-down time<input readonly value="${this.putDownTime}"></label>
 				</form>
