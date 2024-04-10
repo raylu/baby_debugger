7b102090846dd538a4bbeda2c50231f22caae0cc
raylu <90059+raylu@users.noreply.github.com>
2024-04-09 21:16:06 -0700

    handle assertion.response properties
    mobile browsers do not set keys on the response

--- ts/app.ts
+++ ts/app.ts
@@ -108,11 +108,12 @@ class BabyDebugger extends LitElement {
 			'id': assertion.id,
 			'rawId': this._encode_base64(assertion.rawId),
 			'type': assertion.type,
-			'response': Object.fromEntries(Object.entries(assertion.response).map(
-					([k, v]) => [k, this._encode_base64(v)])),
+			'response': {},
 			'authenticatorAttachment': assertion.authenticatorAttachment,
 			'clientExtensionResults': assertion.getClientExtensionResults(),
 		}
+		for (const prop in assertion.response)
+			assertObj['response'][prop] = this._encode_base64(assertion.response[prop]);
 		const assertionResponse = await this._post_json('/api/login/assert',
 				{'username': username, 'assertion': assertObj});
 		if (assertionResponse.ok) {
